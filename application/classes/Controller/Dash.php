43fa9aadbfd144e1ea29afc3c897028295f9aac1
KalevAnnuk <kalev.annuk@khk.ee>
2013-01-22 17:30:29 +0200

    Basic Setup

---
+++ application/classes/Controller/Dash.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Dash extends Controller_Main
+{
+    public function action_index()
+    {
+        $this->template->content = View::factory('dash/index');
+    }
+}
